bccfb4e71007af3ab65b532972cc79453a29f92b
Asamoah Yeboah Isaac <asamoah1357@gmail.com>
2023-05-27 05:03:36 -0700

    git

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import {Inter} from 'next/font/google'
+import DatePicker from '@/components/DatePicker'
 
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
         <main>
-            Hello, World
+            <DatePicker/>
         </main>
     )
 }
