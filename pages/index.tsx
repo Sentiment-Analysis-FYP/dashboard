9c04985eb7a3b97a3210816dc9776be4c33f7b2b
derrick <dkkusi2010@gmail.com>
2023-06-21 14:41:33 +0000

    added framer button animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,10 @@ import DatePicker from "@/components/DatePicker";
 import AddKeyword from "@/components/AddKeyword";
 import NewDatePicker from "@/components/NewDatePicker";
 import Link from "next/link";
+import { CgRename } from "react-icons/cg";
+import { AiOutlineMail } from "react-icons/ai";
+import { BiLock } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 const nunito = Nunito({
   weight: ["400"],
@@ -42,10 +46,10 @@ export default function Home() {
           />
         </div>
 
-        <div className="z-[100] flex flex-col justify-center items-center w-1/2">
-          <div className="flex flex-col text-sm">
-            <span className="text-gray-900 text-2xl">Sign In</span>
-            <span className="text-sm">If you don't have an account</span>
+        <div className="z-[100] flex flex-col justify-center items-center w-1/2 gap-8">
+          <div className="flex flex-col text-sm w-96">
+            <span className="text-gray-900 text-2xl pb-4">Sign In</span>
+            <span className="text-sm pb-2">If you don't have an account</span>
             <span className="">
               You can
               <Link
@@ -57,12 +61,67 @@ export default function Home() {
               </Link>
             </span>
           </div>
-          <div>
-            sign in. If you don't have an account, you can register here
+
+          <div className="flex flex-col gap-3 w-96">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name" className="text-sm">
+                Name
+              </label>
+              <div className="border-b-[1px] border-indigo-600 flex gap-2 items-center">
+                <CgRename size={14} />
+                <input
+                  type="text"
+                  name="name"
+                  className="outline-none"
+                  placeholder="Enter your full name"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email" className="text-sm">
+                Email
+              </label>
+              <div className="border-b-[1px] border-indigo-600 flex gap-2 items-center">
+                <AiOutlineMail size={14} />
+                <input
+                  type="text"
+                  name="email"
+                  className="outline-none"
+                  placeholder="Enter your email address"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="password" className="text-sm">
+                Password
+              </label>
+              <div className="border-b-[1px] border-indigo-600 flex gap-2 items-center">
+                <BiLock size={14} />
+                <input
+                  type="password"
+                  name="password"
+                  className="outline-none"
+                  placeholder="Enter a strong password"
+                />
+              </div>
+            </div>{" "}
+            <div className="flex justify-between text-xs w-96">
+              <div className="flex gap-1">
+                <input type="checkbox" />
+                <span>Remember me</span>
+              </div>
+              <div>
+                <Link href={"/resetpassword"}>Forgot Password</Link>
+              </div>
+            </div>
           </div>
-          <div> Name and input Email and input Password and input</div>
-          <div> remembrer me button and forgotten password </div>
-          <div>login button</div>
+
+          <motion.button className="bg-indigo-500 h-12 w-96 flex justify-center items-center rounded-full text-white"
+          whileTap={{
+            scale: 0.95
+          }}>
+            Login
+          </motion.button>
         </div>
       </div>
     </main>
