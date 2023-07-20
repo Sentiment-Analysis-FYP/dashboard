b1acf6a112e9ef3be9d8e27c6e59fe3b7ff5e67e
Max Otuteye <maxotuteye12@gmail.com>
2023-07-20 20:45:17 +0000

    auth sign in util

---
+++ utils/auth.ts
@@ -0,0 +1,29 @@
+export const storeUserLogin = (email: string, token: string) => {
+    window.sessionStorage.setItem('email', email)
+    window.sessionStorage.setItem('token', token)
+}
+export const signIn = async (email: string, password: string) => {
+    let status = 400
+    await fetch(`${process.env.BASE_URL}/api/auth/signin`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        body: JSON.stringify({
+            email: `${email}`,
+            password: `${password}`
+        })
+    }).then(response => {
+        status = response.status
+        console.log(status)
+        return response.json()
+    }).then(data => {
+        console.log(data)
+        storeUserLogin(email, data.accessToken)
+    }).catch(err => {
+        console.log(err)
+    })
+    return status
+}
