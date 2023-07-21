360abdb186e6f3e440bdce51926d2321a64abd1c
Max Otuteye <maxotuteye12@gmail.com>
2023-07-21 01:18:44 +0000

    cleaned comments

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Link from 'next/link'
-import {FaFacebookF, FaInstagram, FaLinkedin, FaTwitter} from "react-icons/fa";
-import {AiOutlineMenu} from "react-icons/ai";
-import {GrFormClose} from "react-icons/gr";
-import {useAuth} from "@/hooks/auth";
+import {useAuth} from "@/hooks/auth"
 
 const Header = () => {
     const [email, token] = useAuth()
@@ -16,50 +13,7 @@ const Header = () => {
         {name: "Help", slug: "help"},
     ]
 
-    const [categories, setCategories] = useState(stockCategories);
-    //
-    // useEffect(() => {
-    //     setemail(useAuth().email)
-    // }, []);
-    //
-    // useEffect(() => {
-    //     if (email) {
-    //         stockCategories.shift()
-    //         setCategories(stockCategories)
-    //     }
-    // }, [email]);
-
-    // useEffect(() => {
-    //     const {email, token} = useAuth()
-    //     // setUpdateHeader(false)
-    //     setemail(email)
-    // }, [updateHeader]);
-
-
-    const fadeInTopRight = "opacity-100 duration-150 scale-100 ease-in-out delay-75"
-    const fadeOutTopRight = "opacity-0 invisible duration-100 scale-50 translate-x-40 -translate-y-32"
-
-    const [mobileMenu, setMobileMenu] = useState(false);
-
-    const toggleMobileMenu = () => {
-        setMobileMenu(!mobileMenu)
-    }
-
-    // const UserProfile = () => {
-    //     return (
-    //         <Link href='/login'>
-    //             <div className="rounded-full w-10 h-10 text-white bg-orange-500 uppercase
-    //         text-2xl flex justify-center items-center absolute right-20 shadow-md
-    //         cursor-pointer hover:bg-orange-600 transition duration-300 hover:shadow-lg">
-    //
-    //                 <div className='flex justify-center items-center pb-[2px]'>
-    //                     {email.charAt(0)}
-    //                 </div>
-    //
-    //             </div>
-    //         </Link>
-    //     )
-    // }
+    const [categories, setCategories] = useState(stockCategories)
 
     return (
         <div className="fixed top-0 left-0 bg-white z-50 h-22 w-full shadow-lg">
@@ -73,7 +27,7 @@ const Header = () => {
                                 </span>
                             </Link>
                         </div>
-                        <div className="flex items-center justify-center md:contents ">
+                        <div className="flex items-center justify-center">
                             <div className="flex gap-16 mx-16 justify-center items-center p-1 w-full">
                                 {categories.map((category) => (
                                     <Link key={category.slug} href={`/${category.slug}`}>
@@ -86,66 +40,10 @@ const Header = () => {
                                     </Link>
                                 ))}
                             </div>
-                            {/*<div className="pl-16 pt-1 flex gap-4 float-right justify-center items-center">*/}
-                            {/*    <Link href="">*/}
-                            {/*        <div className="cursor-pointer hover:text-blue-900 transition*/}
-                            {/*         flex items-center duration-100 ease-in-out">*/}
-                            {/*            <FaFacebookF size={22}/>*/}
-                            {/*        </div>*/}
-                            {/*    </Link>*/}
-
-                            {/*    <Link href="">*/}
-                            {/*        <div className="cursor-pointer hover:text-blue-900 transition*/}
-                            {/*         flex items-center duration-100 ease-in-out">*/}
-                            {/*            <FaTwitter size={22}/>*/}
-                            {/*        </div>*/}
-                            {/*    </Link>*/}
-
-                            {/*    <Link href="">*/}
-                            {/*        <div className="cursor-pointer hover:text-blue-900 transition*/}
-                            {/*         flex items-center duration-100 ease-in-out">*/}
-                            {/*            <FaInstagram size={22}/>*/}
-                            {/*        </div>*/}
-                            {/*    </Link>*/}
-
-                            {/*    <Link href="">*/}
-                            {/*        <div className="cursor-pointer hover:text-blue-900 transition*/}
-                            {/*         flex items-center duration-100 ease-in-out">*/}
-                            {/*            <FaLinkedin size={22}/>*/}
-                            {/*        </div>*/}
-                            {/*    </Link>*/}
-                            {/*</div>*/}
                         </div>
                     </div>
                 </div>
-                {/*{email && !mobile ? (<UserProfile/>) : (<div></div>)}*/}
-
             </div>
-            {/*<div*/}
-            {/*    className={(mobile ? " opacity-100 " : " invisible ") + "absolute top-1 right-4 rounded-full w-16 h-16 " +*/}
-            {/*        "flex justify-center text-black items-center cursor-pointer"} onClick={toggleMobileMenu}>*/}
-            {/*    <AiOutlineMenu size={32}/>*/}
-            {/*</div>*/}
-            {/*<div*/}
-            {/*    className={mobileMenu ? "opacity-100 flex visible flex-col items-center fixed top-0 left-0 w-full p-4" +*/}
-            {/*        " bg-transparent overflow-hidden z-50 " + fadeInTopRight :*/}
-            {/*        " flex flex-col items-center fixed top-0 left-0 w-full p-4 bg-transparent overflow-hidden z-50 " +*/}
-            {/*        fadeOutTopRight}>*/}
-            {/*    <div className="pl-4 bg-gray-100 w-full overflow-hidden rounded-sm">*/}
-            {/*        {categories.map((category) => (*/}
-            {/*            <Link key={category.slug} href={`/${category.slug}`}>*/}
-            {/*                <div key={category.slug} className="border-b-[1px] border-gray-200 flex*/}
-            {/*            items-center pl-2 py-6 text-lg text-black cursor-pointer">*/}
-            {/*                    {category.name}*/}
-            {/*                </div>*/}
-            {/*            </Link>*/}
-            {/*        ))}*/}
-            {/*    </div>*/}
-            {/*    <div className="fixed top-6 right-10 rounded-full w-16 h-16*/}
-            {/*        flex justify-center items-center opacity-80" onClick={toggleMobileMenu}>*/}
-            {/*        <GrFormClose size={36}/>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     )
 }
