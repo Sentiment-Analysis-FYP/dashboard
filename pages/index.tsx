95c4d7b496e456ef539a33344dbeeedcac6bd017
Max Otuteye <maxotuteye12@gmail.com>
2023-07-21 01:11:40 +0000

    cleaned comments in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
-import Image from "next/image";
 import {Nunito} from "next/font/google";
-import UserName from "@/components/UserName";
-import BeginScrape from "@/components/BeginScrape";
-import DatePicker from "@/components/DatePicker";
-import AddKeyword from "@/components/AddKeyword";
-import NewDatePicker from "@/components/NewDatePicker";
-import Link from "next/link";
-import {CgRename} from "react-icons/cg";
-import {AiOutlineMail} from "react-icons/ai";
-import {BiLock} from "react-icons/bi";
-import {motion} from "framer-motion";
 import {SignUp} from "@/components/SignUp";
-import {SignIn} from "@/components/SignIn";
 import Header from "@/components/Header";
 import {useAuth} from "@/hooks/auth";
 
