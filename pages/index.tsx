554087bde6471a2fc1ab4cb10ec6c4dcc1747dcb
Max Otuteye <maxotuteye12@gmail.com>
2023-07-23 11:51:52 +0000

    fixed home margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
     const HomePage = () => {
         return (
-            <div>
+            <div className='mt-32'>
                 Home Page
             </div>
         )
