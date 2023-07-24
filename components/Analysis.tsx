5172b803c4861a1f0c2ba5d82ebd13ce35bd6893
Max Otuteye <maxotuteye12@gmail.com>
2023-07-24 22:19:51 +0000

    finish analysis components

--- components/Analysis.tsx
+++ components/Analysis.tsx
@@ -107,7 +107,6 @@ const Analysis = (props: AnalysisProps) => {
                             </div>
                         </Link>
                     </div>
-
                     {data ?
                         (<div className='w-full h-full flex justify-center items-center'>
                             <DataTable data={data}/>
