f001f8ab1eca19e2ca9e79baf23f995a1da0c68d
Max Otuteye <maxotuteye12@gmail.com>
2023-07-25 16:59:29 +0000

    using wordcloud for custom wordcloud

--- components/charts/CustomWordCloud.tsx
+++ components/charts/CustomWordCloud.tsx
@@ -1,7 +1,27 @@
-const CustomWordCloud = () => {
+import {useLayoutEffect} from "react";
+import WordCloud from "wordcloud";
+import {number} from "prop-types";
+
+interface CustomWordCloudProps {
+    wordList: [string, number][]
+}
+
+const CustomWordCloud = (props: CustomWordCloudProps) => {
+    const {wordList} = props
+
+    useLayoutEffect(() => {
+        if (document && document.getElementById('custom-wordcloud'))
+            // draw cloud
+            WordCloud(document.getElementById('custom-wordcloud')!, {
+                list: wordList
+            })
+    }, [])
+
+
     return (
-        <div className=''>
+        <div className='w-full'>
             word cloud
+            <canvas id='custom-wordcloud' width='100%' height='100%'/>
         </div>
     )
 }
