ca79cb2e60e94fcc4ad62ba662a1f9da219f4425
Max Otuteye <maxotuteye12@gmail.com>
2023-07-26 14:23:06 +0000

    added socket component

---
+++ components/WebSocketHandler.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+
+const WebSocketComponent = () => {
+    const [isComplete, setIsComplete] = useState(false);
+
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:4000'); // Replace with your backend URL
+
+        ws.onopen = () => {
+            console.log('WebSocket connection established');
+        };
+
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            setIsComplete(data.isComplete); // Update state based on data received from the backend
+        };
+
+        ws.onclose = () => {
+            console.log('WebSocket connection closed');
+        };
+
+        return () => {
+            // Clean up the WebSocket connection when the component unmounts
+            ws.close();
+        };
+    }, []);
+
+    return (
+        <div>
+            {isComplete ? <p>Task is complete!</p> : <p>Task is not complete yet.</p>}
+        </div>
+    );
+};
+
+export default WebSocketComponent
