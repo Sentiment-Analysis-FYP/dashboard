53f5490481c53c704c3da12a7ed4d5c45d11cbc0
Max Otuteye <maxotuteye12@gmail.com>
2023-08-14 03:16:11 +0000

    fixing for vercel build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <title>Sentinel - Large Data Analysis</title>
+                <title>FeBa - Large Data Analysis</title>
             </Head>
             <body>
             <Main/>
